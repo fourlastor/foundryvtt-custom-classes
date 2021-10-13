f61fa70adbe643c1cc0249f8d43608104c7c1433
Daniele Conti <fourlastor@users.noreply.github.com>
2021-10-13 10:02:21 +0100

    Base implementation
    Showing existing classes

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,55 @@
-// Hooks.once('init', async function() {
+function log(data) {
+  console.log("Foundry VTT | Custom Classes", data)
+}
 
-// });
+Hooks.once('ready', async function () {
+  log(CONFIG.DND5E.classFeatures);
 
-Hooks.once('ready', async function() {
-    console.log(arguments)
+  game.settings.registerMenu("myModule", "mySettingsMenu", {
+    name: "Custom Classes",
+    label: "Manage custom classes",      // The text label used in the button
+    hint: "Manage custom classes.",
+    icon: "fas fa-cogs",               // A Font Awesome icon used in the submenu button
+    type: MySubmenuApplicationClass,   // A FormApplication subclass
+    restricted: true                   // Restrict this submenu to gamemaster only?
+  });
+  //   await game.settings.register('myModuleName', 'myComplexSettingName', {
+  //     scope: 'world',     // "world" = sync to db, "client" = local storage
+  //     config: false,      // we will use the menu above to edit this setting
+  //     type: Object,
+  //     default: {},        // can be used to set up the default structure
+  //   });
 });
+
+
+
+
+
+/**
+ * For more information about FormApplications, see:
+ * https://foundryvtt.wiki/en/development/guides/understanding-form-applications
+ */
+class MySubmenuApplicationClass extends FormApplication {
+  // lots of other things...
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      template: "modules/foundryvtt-custom-classes/templates/settings.hbs",
+      title: "CUSTOM_CLASSES.CustomClassesTitle",
+      width: 800,
+      height: "auto",
+      resizable: true,
+    });
+  }
+
+  getData() {
+    return {
+      classFeatures: CONFIG.DND5E.classFeatures,
+    } // game.settings.get('myModuleName', 'myComplexSettingName');
+  }
+
+  _updateObject(event, formData) {
+    const data = expandObject(formData);
+    log(data);
+    // game.settings.set('myModuleName', 'myComplexSettingName', data);
+  }
+}
