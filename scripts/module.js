0b3f8795f8292bd1a20066a05a453a35a53c5e98
Daniele Conti <fourlastor@users.noreply.github.com>
2021-10-13 14:13:24 +0100

    Separate data layer

--- scripts/module.js
+++ scripts/module.js
@@ -1,11 +1,17 @@
+const MODULE_NAME = 'foundryvtt-custom-classes'
+const SETTING_NAME = 'classesOverride'
+
 function log(data) {
   console.log("Foundry VTT | Custom Classes", data)
 }
 
 Hooks.once('ready', async function () {
-  const originalClassFeatures = CONFIG.DND5E.classFeatures
-  log(originalClassFeatures)
-  CustomClassesApplication.originalClassFeatures = originalClassFeatures
+  game.settings.register(MODULE_NAME, SETTING_NAME, {
+    scope: 'world',
+    config: false,
+    type: Object,
+    default: convertToData(CONFIG.DND5E.classFeatures),
+  })
 
   game.settings.registerMenu("myModule", "mySettingsMenu", {
     name: "Custom Classes",
@@ -15,17 +21,39 @@ Hooks.once('ready', async function () {
     type: CustomClassesApplication,   // A FormApplication subclass
     restricted: true                   // Restrict this submenu to gamemaster only?
   })
-  //   await game.settings.register('myModuleName', 'myComplexSettingName', {
-  //     scope: 'world',     // "world" = sync to db, "client" = local storage
-  //     config: false,      // we will use the menu above to edit this setting
-  //     type: Object,
-  //     default: {},        // can be used to set up the default structure
-  //   });
 });
 
-class CustomClassesApplication extends FormApplication {
+function convertToData(classFeatures) {
+  return {
+    classes: names(classFeatures).map(className => {
+      const clazz = classFeatures[className]
+      return {
+        name: className,
+        paths: names(clazz.subclasses).map(subclassName => {
+          const subclass = clazz.subclasses[subclassName]
+          const features = subclass.features;
+          return {
+            name: subclassName,
+            levels: names(features).map(level => ({
+              level: level,
+              features: features[level],
+            }))
+          }
+        }),
+      }
+    })
+  }
+}
 
-  static originalClassFeatures
+function names(originalObj) {
+  const names = []
+  for (let it in originalObj) {
+    names.push(it)
+  }
+  return names
+}
+
+class CustomClassesApplication extends FormApplication {
 
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
@@ -38,38 +66,7 @@ class CustomClassesApplication extends FormApplication {
   }
 
   getData() {
-    // game.settings.get('myModuleName', 'myComplexSettingName');
-    return this.convertToData(CONFIG.DND5E.classFeatures)
-  }
-
-  convertToData(classFeatures) {
-    return {
-      classes: this.names(classFeatures).map(className => {
-        const clazz = classFeatures[className]
-        return {
-          name: className,
-          paths: this.names(clazz.subclasses).map(subclassName => {
-            const subclass = clazz.subclasses[subclassName]
-            const features = subclass.features;
-            return {
-              name: subclassName,
-              levels: this.names(features).map(level => ({
-                level: level,
-                features: features[level],
-              }))
-            }
-          }),
-        }
-      })
-    }
-  }
-
-  names(originalObj) {
-    const names = []
-    for (let it in originalObj) {
-      names.push(it)
-    }
-    return names
+    return game.settings.get(MODULE_NAME, SETTING_NAME);
   }
 
   _updateObject(event, formData) {
