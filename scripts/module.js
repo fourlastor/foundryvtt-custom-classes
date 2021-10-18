135e76a26c147d75a21931d930a96c60f48657fb
Daniele Conti <fourlastor@users.noreply.github.com>
2021-10-18 12:16:23 +0100

    wip

--- scripts/module.js
+++ scripts/module.js
@@ -10,48 +10,18 @@ Hooks.once('ready', async function () {
     scope: 'world',
     config: false,
     type: Object,
-    default: convertToData(CONFIG.DND5E.classFeatures),
+    default: CONFIG.DND5E.classFeatures,
   })
 
   game.settings.registerMenu("myModule", "mySettingsMenu", {
     name: "Custom Classes",
-    label: "Manage custom classes",      // The text label used in the button
+    label: "Manage custom classes",
     hint: "Manage custom classes.",
-    icon: "fas fa-cogs",               // A Font Awesome icon used in the submenu button
-    type: CustomClassesApplication,   // A FormApplication subclass
-    restricted: true                   // Restrict this submenu to gamemaster only?
+    icon: "fas fa-cogs",
+    type: CustomClassesApplication,
+    restricted: true,
   })
-});
-
-function convertToData(classFeatures) {
-  return {
-    classes: names(classFeatures).map(className => {
-      const clazz = classFeatures[className]
-      return {
-        name: className,
-        paths: names(clazz.subclasses).map(subclassName => {
-          const subclass = clazz.subclasses[subclassName]
-          const features = subclass.features;
-          return {
-            name: subclassName,
-            levels: names(features).map(level => ({
-              level: level,
-              features: features[level],
-            }))
-          }
-        }),
-      }
-    })
-  }
-}
-
-function names(originalObj) {
-  const names = []
-  for (let it in originalObj) {
-    names.push(it)
-  }
-  return names
-}
+})
 
 class CustomClassesApplication extends FormApplication {
 
@@ -62,16 +32,18 @@ class CustomClassesApplication extends FormApplication {
       width: 800,
       height: "auto",
       resizable: true,
-    });
+    })
   }
 
   getData() {
-    return game.settings.get(MODULE_NAME, SETTING_NAME);
+    const classFeatures = game.settings.get(MODULE_NAME, SETTING_NAME)
+    log(classFeatures)
+    return {classFeatures: classFeatures}
   }
 
   _updateObject(event, formData) {
-    const data = expandObject(formData);
-    log(data);
-    // game.settings.set('myModuleName', 'myComplexSettingName', data);
+    const data = expandObject(formData)
+    log(data)
+    // game.settings.set('myModuleName', 'myComplexSettingName', data)
   }
 }
